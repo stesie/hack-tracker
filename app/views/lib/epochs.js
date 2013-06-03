99c76a02d4023fe625acc5b093867b8de7deb91f
Stefan Siegl <stesie@brokenpipe.de>
2013-06-04 00:04:26 +0200

    Add third epoch.

--- app/views/lib/epochs.js
+++ app/views/lib/epochs.js
@@ -1,8 +1,13 @@
 exports.getEpochFromDoc = function(doc) {
     if(doc.timestamp < 1369944000) {
-	/* before virus introduction */
-	return 0;
+        /* before virus introduction */
+        return 0;
     }
 
-    return 1;
+    if(doc.timestamp < 1370275200) {
+        /* before increased xmp drop from enemy portals */
+        return 1;
+    }
+
+    return 2;
 }
